12fbff51544dce9453c6c0d4e833057a95e9b33d
eithan31sirusi <62159373+eithan31sirusi@users.noreply.github.com>
2020-06-09 19:37:47 +0300

    Updates

---
+++ precache-manifest.e1db27a6e2f14f7a1ae9384fb9ba9853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86d26829a2490a8a6d5903efdb2600d",
+    "url": "/https//eithan31sirusi.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a5a77e3b56d88a0983c6",
+    "url": "/https//eithan31sirusi.github.io/monsters-rolodex/static/css/main.0fb2a265.chunk.css"
+  },
+  {
+    "revision": "8eefb47c5e7c6f1166f6",
+    "url": "/https//eithan31sirusi.github.io/monsters-rolodex/static/js/2.27033893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https//eithan31sirusi.github.io/monsters-rolodex/static/js/2.27033893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a77e3b56d88a0983c6",
+    "url": "/https//eithan31sirusi.github.io/monsters-rolodex/static/js/main.7bdf384a.chunk.js"
+  },
+  {
+    "revision": "9088e127d36b8d113767",
+    "url": "/https//eithan31sirusi.github.io/monsters-rolodex/static/js/runtime-main.bc181780.js"
+  }
+]);
